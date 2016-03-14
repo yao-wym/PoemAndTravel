ae836a15835ccb58dc0540d3e7e7728b6988b6c5
wym <wym@wymdeMacBook-Pro.local>
2016-03-14 08:38:43 +0800

    自渲染测试

--- src/components/common-header/index.js
+++ src/components/common-header/index.js
@@ -2,5 +2,8 @@ require('./style.styl')
 
 module.exports = {
 	template: require('./template.html'),
+	data:function () {
+    return { test: 123 }
+  },
 	props: ['title','leftLabel','rightLabel']
 } 
